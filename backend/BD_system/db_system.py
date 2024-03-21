15f3a45a5c3b0f3627b427058353141cb730b8f4
Maks22242 <makcum.petrenko@mail.ru>
2024-03-21 12:40:16 +0300

    Добавил базу данных

---
+++ backend/BD_system/db_system.py
@@ -0,0 +1,82 @@
+import sqlite3
+
+class MyDatabase:
+    def __init__(self, db_name='mydatabase.db'):
+        self.conn = sqlite3.connect(db_name)
+        self.cursor = self.conn.cursor()
+
+    def create_table(self):
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS mytable (
+                id INTEGER PRIMARY KEY,
+                absolute_path TEXT UNIQUE,
+                hash TEXT,
+                encrypted_hash TEXT NULL,
+                type_hash TEXT,
+                type_encrypted TEXT NULL,
+                extra_info_encryption TEXT NULL,
+                hash_key_encrypted TEXT NULL,
+                body_file BLOB
+            )
+        ''')
+        self.conn.commit()
+
+    def insert_record(self, absolute_path, hash_value, encrypted_hash=None, type_hash=None, type_encrypted=None, extra_info_encryption=None, hash_key_encrypted=None, body_file=None):
+        try:
+            self.cursor.execute('''
+                INSERT INTO mytable (absolute_path, hash, encrypted_hash, type_hash, type_encrypted, extra_info_encryption, hash_key_encrypted, body_file) 
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+            ''', (absolute_path, hash_value, encrypted_hash, type_hash, type_encrypted, extra_info_encryption, hash_key_encrypted, body_file))
+            self.conn.commit()
+            return True
+        except sqlite3.IntegrityError:
+            print("Record with absolute path '{}' already exists.".format(absolute_path))
+            return False
+
+    def get_record_by_absolute_path(self, absolute_path):
+        self.cursor.execute('''
+            SELECT * FROM mytable WHERE absolute_path = ?
+        ''', (absolute_path,))
+        record = self.cursor.fetchone()
+
+        # Разбиваем поле extra_info_encryption на части, если оно есть
+        if record and record[6]:
+            record_extra_info_encryption = record[6].split(',')
+            record = list(record)
+            record[6] = record_extra_info_encryption
+            record = tuple(record)
+
+        return record
+
+    def update_record_by_absolute_path(self, absolute_path, **kwargs):
+        update_query = "UPDATE mytable SET "
+        update_query += ", ".join([f"{key} = ?" for key in kwargs.keys()])
+        update_query += " WHERE absolute_path = ?"
+
+        values = list(kwargs.values())
+        values.append(absolute_path)
+
+        self.cursor.execute(update_query, tuple(values))
+        self.conn.commit()
+
+    def delete_record_by_absolute_path(self, absolute_path):
+        self.cursor.execute('''
+            DELETE FROM mytable WHERE absolute_path = ?
+        ''', (absolute_path,))
+        self.conn.commit()
+
+    def __del__(self):
+        self.conn.close()
+
+if __name__ == "__main__":
+    db = MyDatabase()
+    db.create_table()
+
+    # Пример использования операций CRUD
+    db.insert_record('example_path', 'example_hash', encrypted_hash='example_encrypted_hash', type_hash='example_type_hash',extra_info_encryption = "aaa,aasd,assad")
+    record = db.get_record_by_absolute_path('example_path')
+    print("Retrieved Record:", record)
+    db.update_record_by_absolute_path('example_path', hash='updated_hash')
+    record = db.get_record_by_absolute_path('example_path')
+    print("Updated Record:", record)
+    db.delete_record_by_absolute_path('example_path')
